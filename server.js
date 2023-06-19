ce5e7700ad2552786245a1638d8f35c5c12ade4a
Katta Ganesh <ganeshkatta39@gmail.com>
2023-06-19 21:00:41 +0530

    updating the port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const connectDB = require("./config/dbConnection");
 const app = express();
 const dotenv = require("dotenv").config();
-const port = process.env || 3000;
+const port = process.env.port || 3000;
 
 connectDB();
 app.use(express.json());
